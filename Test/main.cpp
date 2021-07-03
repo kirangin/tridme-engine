780bef658895f73248c697ad3d8bcc13279fee12
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-03 17:55:48 +0700

    basic program

--- Test/main.cpp
+++ Test/main.cpp
@@ -5,5 +5,34 @@
 int main(int argc, char* argv[]) {
 	if (!glfwInit()) { LOG(ERROR, "Cannot Initialize GLFW!"); }
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Tridme Engine", NULL, NULL);
+
+	if (!window) {
+		LOG(ERROR, "Cannot create GLFW window!");
+		return -1;
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		LOG(ERROR, "Cannot load OpenGL process!");
+		return -1;
+		glfwTerminate();
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
