54146c24204bc72fdb10c744cae9a4db32cf7015
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-03 15:11:10 +0700

    moved

---
+++ Includes/core.h
@@ -0,0 +1,18 @@
+#ifndef CORE_H 
+#define CORE_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include <extern/glad/glad.h>
+#include <extern/glm/glm.hpp>
+#include <extern/glm/ext/matrix_transform.hpp>
+#include <extern/glm/ext/matrix_clip_space.hpp>
+#include <extern/glm/gtc/type_ptr.hpp>
+#include <extern/stbi/stb_image.h>
+
+#endif
