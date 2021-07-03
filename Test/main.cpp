b6168e2a69044283d7df0b41c9557f4546b69a95
naufaladrna08 <naufaladrn90@gmail.com>
2021-07-03 14:38:18 +0700

    main tset

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,7 +1,9 @@
 #include <engine.h>
+#include <logging.h>
+#include <extern/GLFW/glfw3.h>
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello World" << std::endl;
+	if (!glfwInit()) { LOG(ERROR, "Cannot Initialize GLFW!"); }
 
 	return 0;
 }
