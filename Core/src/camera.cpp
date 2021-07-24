9afff981b293486c8eb1fa5378951be4ea13cbeb
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-07-24 23:18:39 +0700

    ditambahkan fungsi untuk update vector dan set posisi

--- Core/src/camera.cpp
+++ Core/src/camera.cpp
@@ -1,26 +1,52 @@
 #include <camera.h>
 
-Camera::Camera(GLfloat FOV, GLfloat width, GLfloat height, 
-							 GLfloat nearPlane, GLfloat farPlane, glm::vec3 camPos) {
-	cameraPosition = camPos;
-	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+Camera::Camera(GLfloat width, GLfloat height, GLfloat nearPlane, 
+							 GLfloat farPlane, glm::vec3 camPos) {
+	/* Camera position */
+	m_cameraPosition = camPos;
+	m_cameraFront 	 = glm::vec3(0.0f, 0.0f, -1.0f);
+	m_cameraUp       = glm::vec3(0.0f, 1.0f, 0.0f);
 
-	viewMatrix = glm::lookAt(cameraPosition, cameraFront, cameraUp);
-	projectionMatrix = glm::perspective(FOV, width / height, nearPlane, farPlane);
+	/* Camera settings */
+	zoom		 = 45.0f;
+	yaw      = -90.0f;
+	pitch    = 0.0f;
+	speed    = 2.5f;
+	near     = nearPlane;
+	far      = farPlane;
+	m_width  = width;
+	m_height = height;
+
+	/* Update camera matrix */
+	Update();
 }
-
 
 Camera::~Camera() { }
 
 glm::mat4 Camera::getViewMatrix() {
-	return viewMatrix;
+	return glm::lookAt(m_cameraPosition, m_cameraPosition + m_cameraFront, m_cameraUp);
 }
 
 glm::mat4 Camera::getProjectionMatrix() {
-	return projectionMatrix;
+	return glm::perspective(glm::radians(zoom), (float) m_width / (float) m_height, near, far);
 }
 
 glm::vec3 Camera::getCameraPosition() {
-	return cameraPosition;
+	return m_cameraPosition;
+}
+
+void Camera::SetPosition(glm::vec3 new_pos) {
+	m_cameraPosition = new_pos;
+	Update();
+}
+
+void Camera::Update() {
+	glm::vec3 front;
+  front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+  front.y = sin(glm::radians(pitch));
+  front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+  m_cameraFront = glm::normalize(front);
+  // also re-calculate the Right and Up vector
+  m_cameraRight = glm::normalize(glm::cross(m_cameraFront, m_cameraUp));
+  m_cameraUp    = glm::normalize(glm::cross(m_cameraRight, m_cameraFront));
 }
--- Includes/camera.h
+++ Includes/camera.h
@@ -5,17 +5,37 @@
 
 class Camera {
 	public:
-		Camera(GLfloat FOV, GLfloat width, GLfloat height, GLfloat nearPlane, GLfloat farPlane, glm::vec3 camPos);
+		Camera(GLfloat width, GLfloat height, GLfloat nearPlane, GLfloat farPlane, glm::vec3 camPos);
 		~Camera();
 
 		glm::mat4 getViewMatrix();
 		glm::mat4 getProjectionMatrix();
 		glm::vec3 getCameraPosition();
 
+		void SetPosition(glm::vec3 new_pos);
+
+		/* Camera settings */
+    float speed;
+    float sensitivity;
+    float zoom;
+    float yaw;
+    float pitch;
+    float near;
+    float far;
+
+    glm::vec3 m_cameraFront;
+    glm::vec3 m_cameraUp;
+    glm::vec3 m_cameraRight;
+	
 	private:
-		glm::mat4 viewMatrix;
-		glm::mat4 projectionMatrix;
-		glm::vec3 cameraPosition;
+		glm::mat4 m_viewMatrix;
+		glm::mat4 m_projectionMatrix;
+		glm::vec3 m_cameraPosition;
+
+		float m_width;
+    float m_height;
+
+		void Update();
 };
 
 #endif
