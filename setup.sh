c771179743702c0ed44d1405623dc22dec1e8c96
naufaladrna08 <naufaladrn90@gmail.com>
2022-04-01 22:16:29 +0700

    update dependencies

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ OS_NAME=$(grep '^NAME' /etc/os-release)
 
 if [[ $OS_NAME == 'NAME="Ubuntu"' ]]
 then
-	sudo apt install xorg-dev libglu1-mesa-dev libglfw3-dev cmake -y
+	sudo apt install xorg-dev libglu1-mesa-dev build-essential cmake -y
 fi
 
 # TODO: Download file for other distro
