d4cc0eeabff00d855511d5ac0eaa284d24d62685
naufaladrna08 <naufaladrn90@gmail.com>
2022-07-11 09:34:02 +0700

    implemented logging

--- core/include/logging.h
+++ core/include/logging.h
@@ -12,27 +12,7 @@ namespace Tridme {
 	#define ERROR   2
 	#define INFO    3
 
-	void LOG(int type, std::string s) {
-		/* Get Time */
-		std::time_t time = std::time(0);
-		std::tm* tm      = std::localtime(&time);
-
-		std::string _type;
-
-		switch (type) {
-			case SUCCESS: _type = "SUCCESS"; break;
-			case WARNING: _type = "WARNING"; break;
-			case ERROR: _type = "ERROR"; break;
-			case INFO: _type = "INFO"; break;
-		}
-
-		/**
-		 * Example Output format: 
-		 * [18:29:22, ERROR]: Hello World
-		 **/
-		std::cout << "["<< tm->tm_hour << ":" << tm->tm_min << ":" << tm->tm_sec << ", " 
-							<< _type << "]: " << s << std::endl;	
-	}
+	void LOG(int type, std::string s);
 	
 }
 
